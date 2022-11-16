945f0fd0a54f00586effe2993156fbf1eae0b882
Rolo Veгified ✔️ <rolando836@revature.net>
2022-11-16 01:02:00 -0500

    Added my first console app from Visual Studio Code, "RoloHelloWorld"

---
+++ FirstConsoleApp/RoloHelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace RoloHelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world, I did it! My name is Rolando, but please call me Rolo!");
+    }
+}
